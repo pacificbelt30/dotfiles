cda52cebd8afc5388a409f7e406904aaa2224d4c
pacificbelt30 <57101176+pacificbelt30@users.noreply.github.com>
2022-03-01 17:24:55 +0900

    setup.shに出力を追加

--- setup.sh
+++ setup.sh
@@ -4,10 +4,14 @@
 #Plugインストール
 #cd ~/dotfiles
 #chsh -s $(which zsh)
+echo "vim-plugのインストール"
+echo "Install vim-plug"
 zsh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
 
 #make directry
+echo "設定ファイルを配置するディレクトリを作成"
+echo "Create a directory to place the configuration files."
 makedir () {
   if [ ! -e $HOME/$1 ]; then
     mkdir $HOME/$1
@@ -29,13 +33,19 @@ makedir .config/nvim/colors
 #makedir .config/nvim/snippets
 
 #$HOME 以下の設定ファイル
+echo "設定ファイルのリンク貼り付け開始($HOME以下)"
+echo "Start pasting the link to the configuration file (under $HOME)."
 ln -snf `pwd`/vimrc $HOME/.vimrc
 ln -snf `pwd`/co.vim $HOME/.vim/co.vim
 ln -snf `pwd`/tmux.conf $HOME/.tmux.conf
 #ln -snf `pwd`/p10k.zsh $HOME/.p10k.zsh
 ln -snf `pwd`/latexmkrc $HOME/.latexmkrc
+echo "リンク貼り付け終了($HOME以下)"
+echo "End pasting the link to the configuration file (under $HOME)."
 
 #$HOME/.config 以下の設定ファイル
+echo "リンク貼り付け開始($HOME/.config/以下)"
+echo "Start pasting the link to the configuration file (under $HOME/.config/)."
 ln -snf `pwd`/nvim/init.vim $HOME/.config/nvim/init.vim
 #ln -snf `pwd`/nvim/colors/gruvbox.vim $HOME/.config/nvim/colors/gruvbox.vim
 #ln -snf `pwd`/nvim/colors $HOME/.config/nvim/colors
@@ -70,9 +80,13 @@ ln -snf `pwd`/i3wm-setup/.config/picom.conf $HOME/.config/compton/picom.conf
 ln -snf `pwd`/i3wm-setup/.config/xfce4/terminal/terminalrc $HOME/.config/xfce4/terminal/terminalrc
 ln -snf `pwd`/i3wm-setup/.config/xfce4/terminal/accels.scm $HOME/.config/xfce4/terminal/accels.scm
 ln -snf `pwd`/i3wm-setup/.config/dunst/dunstrc $HOME/.config/dunst/dunstrc
+echo "リンク貼り付け終了($HOME/.config/以下)"
+echo "End pasting the link to the configuration file (under $HOME/.config/)."
 
 
 #zprezto
+echo "preztoの設定"
+echo "setup prezto"
 git clone --recursive https://github.com/sorin-ionescu/prezto.git "${ZDOTDIR:-$HOME}/.zprezto"
 
 setopt EXTENDED_GLOB
