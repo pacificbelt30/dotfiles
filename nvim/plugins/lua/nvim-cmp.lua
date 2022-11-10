b4835e5d5d7e8d7f9f7020e01a6a0ea00b31cc09
pacificbelt30 <57101176+pacificbelt30@users.noreply.github.com>
2022-11-11 01:29:55 +0900

    update nvim-cmp config

--- nvim/plugins/lua/nvim-cmp.lua
+++ nvim/plugins/lua/nvim-cmp.lua
@@ -91,7 +91,8 @@ cmp.setup.cmdline(':', {
 })
 
 -- Setup lspconfig.
-local capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities())
+local capabilities = require('cmp_nvim_lsp').default_capabilities(vim.lsp.protocol.make_client_capabilities())
+-- local capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities())
 require("mason").setup()
 require("mason-lspconfig").setup({
   ensure_installed = { "pyright", "texlab" }
